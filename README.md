83349d12910edb1ce9f2b5c55dd591179265f193
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-01-28 19:45:15 -0600

    Initial Project Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Entropy and Complexity
+
+This is an entry into Global Game Jam Austin 2022 with the theme "Duality".
---
+++ Source/EntropyAndComplexity/EntropyAndComplexityGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EntropyAndComplexityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTROPYANDCOMPLEXITY_API AEntropyAndComplexityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
