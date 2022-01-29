78e99ad56b6fa633938cb09a7f6b1c3e03325c7a
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-01-29 15:26:49 -0600

    Prepare for Unity

--- Source/EntropyAndComplexity/EntropyAndComplexity.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "EntropyAndComplexity.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EntropyAndComplexity, "EntropyAndComplexity" );
