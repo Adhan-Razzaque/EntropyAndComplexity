b899d57778643b7998ba0286600b157a06b20bac
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-01-30 12:29:41 -0600

    Add Game Over

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int sceneIndex;
+    
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
